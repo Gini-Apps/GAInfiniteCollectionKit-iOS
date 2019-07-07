0a174f571f0c696d2e38377f47f4e60910275b5b
Ido Meirov <idom@gini-apps.com>
2019-07-07 11:50:29 +0300

    update path

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "GAInfiniteCollectionKit",
-            dependencies: []),
+            path: "GAInfiniteCollectionKit-iOS/Classes"),
         .testTarget(
             name: "GAInfiniteCollectionKitTests",
             dependencies: ["GAInfiniteCollectionKit"]),
